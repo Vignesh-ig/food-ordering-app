eca510e2b1ba1c13ba79e834dfd015997625c9ce
vignesh <vigneshkwork7@gmail.com>
2025-08-12 22:58:21 +0530

    Update: Created repository for restaurant.

---
+++ Back-end/Online Food Ordering/Online-Food-Ordering/src/main/java/com/order/module/restaurant/repository/RestaurantRepository.java
@@ -0,0 +1,18 @@
+package com.order.module.restaurant.repository;
+
+import com.order.module.restaurant.entity.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+
+    Restaurant findByOwnerId(Long userId);
+
+    @Query("SELECT r FROM Restaurant r WHERE lower(r.name) LIKE lower(concat('%',:query,'%'))" +
+            "OR lower(r.cuisineType) LIKE lower(concat('%',:query,'%'))")
+    List<Restaurant> findRestaurantBySearchQuery(String query);
+
+}
